4a1737d6f05ddcf1f722d5f6ce1c24d690982d86
CatLe2003 <119800521+CatLe2003@users.noreply.github.com>
2023-12-13 05:01:23 +0700

    add Partial folder

---
+++ src/partial/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" class="main-background">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> 
+        <?php
+          if(isset($page_title))
+          {
+            echo "$page_title";
+          }
+        ?>
+    </title>
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@700;800&family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet"></head>
