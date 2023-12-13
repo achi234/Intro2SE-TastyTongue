0975fea5910536884537adf78f0ff52c5eb8c88f
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2023-12-13 15:59:24 +0700

    Update main login UI
    Changing at base.css, main.css, login, register, verify.php

--- src/login.php
+++ src/login.php
@@ -8,37 +8,35 @@
 
 <body>
     <div class="modal">
+        <div class="authen-form__header">
+            <img src="./assets/image/logo_removebg-2.png" alt="TastyTongue.png" class="authen-header__logo">
+            <h1 class="authen-header__name">Tasty Tongue</h1>
+        </div>
         <!-- Login form -->
-        <form method="post" action="checklogin.php">
-            <div class="authen-form">
-                <div class="authen-form__header">
-                    <img src="./assets/image/logo_removebg-2.png" alt="TastyTongue.png" class="authen-header__logo">
-                    <h1 class="authen-header__name">Tasty Tongue</h1>
+        <div class="authen-form">
+            <div class="authen-form__form">
+                <div class="authen-form__group">
+                    <p class="authen-form__title">Email</p>
+                    <input type="text" class="authen-form__input" placeholder="Enter your email" name="email">
                 </div>
-
-                <div class="authen-form__form">
-                    <div class="authen-form__group">
-                        <input type="text" class="authen-form__input" placeholder="Email Address" name="email">
-                    </div>
-                    <div class="authen-form__group">
-                        <input type="password" class="authen-form__input" placeholder="Password" name="password">
-                    </div>
-                </div>
-
-                <div class="authen-form__controls">
-                        <a href="register.php">
-                            <button type="button" class="btn btn--secondary">Register</button>
-                        </a>
-                        <button type="submit" class="btn btn--primary">Login</button>
-                </div>
-
-                <div class="authen-form__aside">
-                    <p class="authen-form__forgot-text">
-                        <a href="" class="authen-form__forgot-link">Forgot password</a>
-                    </p>  
+                <div class="authen-form__group">
+                    <p class="authen-form__title">Password</p>
+                    <input type="password" class="authen-form__input" placeholder="Enter your password" name="password">
                 </div>
             </div>
-        </form>
+
+            <div class="authen-form__controls">
+                <a href="register.php"class="btn btn--secondary">Register</a>
+                <button type="submit" class="btn btn--primary">Login</button>
+            </div>
+
+            <div class="authen-form__aside">
+                <p class="authen-form__forgot-text">
+                    <a href="" class="authen-form__forgot-link">Forgot password</a>
+                </p>  
+            </div>
+        </div>            
+
     </div>
 </body>
 </html>
