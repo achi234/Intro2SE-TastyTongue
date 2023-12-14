d00d27a29fb85d87ec105e8219119c047ab82189
CatLe2003 <119800521+CatLe2003@users.noreply.github.com>
2023-12-15 01:24:16 +0700

    Add Swal alert
    This is an modal displaying errors or announcement for users

--- src/partial/header.php
+++ src/partial/header.php
@@ -16,3 +16,33 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@700;800&family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet"></head>
+    <script src="assets/js/swal.js"></script>
+    <?php
+         if (isset($_SESSION['status'])) {  
+            echo '<script>';
+            echo 'setTimeout(function() {';
+            echo 'swal({';
+            echo '    title: "Failed",';
+            echo '    text: "' . $_SESSION['status'] . '",';
+            echo '    icon: "error",';
+            echo '});';
+            echo '}, 100);';
+            echo '</script>';
+
+            unset($_SESSION['status']);
+        }
+
+        if (isset($_SESSION['announce'])) {  
+          echo '<script>';
+          echo 'setTimeout(function() {';
+          echo 'swal({';
+          echo '    title: "Success",';
+          echo '    text: "' . $_SESSION['announce'] . '",';
+          echo '    icon: "success",';
+          echo '});';
+          echo '}, 100);';
+          echo '</script>';
+
+          unset($_SESSION['announce']);
+      }
+    ?>
