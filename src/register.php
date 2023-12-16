e47aacf7d9a9b5b811d332020603f4568da12866
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2023-12-16 14:33:01 +0700

    Update register.php

--- src/register.php
+++ src/register.php
@@ -17,35 +17,33 @@
     </div>
     <div class="modal">
         <!-- authen form -->
+        <div class="authen-form__header">
+            <div class="authen-header__logo"></div>
+            <h1 class="authen-header__name">Welcome to Tasty Tongue</h1>
+        </div>
         <form method="post" action="Controller/register.php">
             <div class="authen-form">
-                <div class="authen-form__header">
-                    <h1 class="authen-header__name">Welcome to Tasty Tongue</h1>
-                </div>
-
                 <div class="authen-form__form">
-                    <!-- <div class="authen-form__group">
-                        <input type="text" class="authen-form__input" placeholder="Username">
-                    </div> -->
                     <div class="authen-form__group">
-                        <input type="text" class="authen-form__input" placeholder="Fullname" name="fullname">
+                        <p class="authen-form__title">Fullname</p>
+                        <input type="text" class="authen-form__input" placeholder="Enter your fullname" name="fullname">
                     </div>
                     <div class="authen-form__group">
-                        <input type="text" class="authen-form__input" placeholder="Phone Number" name="phone">
+                        <p class="authen-form__title">Email</p>
+                        <input type="text" class="authen-form__input" placeholder="Enter your email" name="phone">
                     </div>
                     <div class="authen-form__group">
-                        <input type="text" class="authen-form__input" placeholder="Email Address" name="email">
+                        <p class="authen-form__title">Password</p>
+                        <input type="password" class="authen-form__input" placeholder="Enter your password" name="email">
                     </div>
                     <div class="authen-form__group">
-                        <input type="password" class="authen-form__input" placeholder="Password" name="password">
+                        <p class="authen-form__title">Confirm password</p>
+                        <input type="password" class="authen-form__input" placeholder="Re-enter your password" name="password">
                     </div>
-                    
                 </div>
-
+    
                 <div class="authen-form__controls">
-                    <a href="login.php"><button type="button" class="btn btn--secondary">Back</button>
-                    </a>
-                    
+                    <a href="login.php" class="btn btn--secondary">Back</a>
                     <button type="submit" name="btn-register" class="btn btn--primary">Register</button>
                 </div>
             </div>
