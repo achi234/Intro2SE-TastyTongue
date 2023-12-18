d4394244b5a6c3099785403a4d4713de3c47bbfa
CatLe2003 <119800521+CatLe2003@users.noreply.github.com>
2023-12-18 19:03:12 +0700

    Update checklogin controller with password hashing

--- src/Controller/checklogin.php
+++ src/Controller/checklogin.php
@@ -12,56 +12,66 @@
             $email = stripslashes($email);
             $password = stripslashes($password);
 
-            $sql="SELECT * FROM users WHERE email='$email' and password='$password' LIMIT 1";
+            $sql="SELECT * FROM users WHERE email='$email' LIMIT 1";
             $result = mysqli_query($conn, $sql);
-
-
-            if(mysqli_num_rows($result) > 0)
+            $row = mysqli_fetch_assoc($result);
+            $hashedPassword = $row['password'];
+            
+            if(!password_verify($password, $hashedPassword))
             {
-                $row=mysqli_fetch_array($result);
-                $count=mysqli_num_rows($result);
-
-                if($row['verify_status'] == '1')
-                {
-                    $_SESSION['authenticated'] = true;
-                    $_SESSION['auth_user'] = 
-                    [
-                        'email' => $row['email'],
-                        'phone' => $row['phone'],
-                        'fullname' => $row['fullname'],
-                        'role' => $row['role'],
-                    ];
-
-                    switch($_SESSION['auth_user']['role'])
-                    {
-                        case 'Admin':
-                            header("location: ../Admin UI/dashboard.php");
-                            exit(0);
-                        case 'Customer':
-                            header("location: ../Customer UI/customerHomepage.php");
-                            exit(0);
-                        case 'Staff':
-                            header("location: ../Staff UI/staffHomepage.php");
-                            exit(0);
-                        default:
-                            $_SESSION['status'] = 'Error when directing...Please try again!';
-                            header("location: ../login.php");
-                            exit(0);
-                    }
-
-                }
-                else
-                {
-                    $_SESSION['status'] = 'Please verify your email address to login.';
-                    header("location: ../login.php");
-                    exit(0);
-                }
+                $_SESSION['status'] = 'Wrong Email or Password. Please enter again';
+                header("location: ../login.php");
+                exit(0);               
             }
             else
             {
-                $_SESSION['status'] = 'Invalid Email or Password.';
-                header("location: ../login.php");
-                exit(0);
+                if(mysqli_num_rows($result) > 0)
+                {
+                    $count=mysqli_num_rows($result);
+
+                    // echo $row['verify_status'];
+                    if($row['verify_status'] == 1)
+                    {
+                        $_SESSION['authenticated'] = true;
+                        $_SESSION['auth_user'] = 
+                        [
+                            'email' => $row['email'],
+                            'phone' => $row['phone'],
+                            'fullname' => $row['fullname'],
+                            'role' => $row['role'],
+                        ];
+
+                        switch($_SESSION['auth_user']['role'])
+                        {
+                            case 'Admin':
+                                header("location: ../Admin UI/dashboard.php");
+                                exit(0);
+                            case 'Customer':
+                                header("location: ../Customer UI/customerHomepage.php");
+                                exit(0);
+                            case 'Staff':
+                                header("location: ../Staff UI/staffHomepage.php");
+                                exit(0);
+                            default:
+                                $_SESSION['status'] = 'Error when directing...Please try again!';
+                                header("location: ../login.php");
+                                exit(0);
+                        }
+
+                    }
+                    else
+                    {
+                        $_SESSION['status'] = 'Please verify your email address to login.';
+                        header("location: ../login.php");
+                        exit(0);
+                    }
+                }
+                else
+                {
+                    $_SESSION['status'] = 'Invalid Email or Password.';
+                    header("location: ../login.php");
+                    exit(0);
+                }
             }
         }
         else
@@ -70,6 +80,12 @@
             header("location: ../login.php");
             exit(0);
         }
+    }
+    else
+    {
+        $_SESSION['status'] = 'Please click login button!';
+        header("location: ../login.php");
+        exit(0);
     }
     // if($count > 0){
 
