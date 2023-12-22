0316df22244af160408cf5de12c251c061949c46
npt5767 <141230403+npt5767@users.noreply.github.com>
2023-12-22 20:13:14 +0700

    Update main.sql

--- main.sql
+++ main.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 13, 2023 lúc 09:20 PM
+-- Thời gian đã tạo: Th12 22, 2023 lúc 02:12 PM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.1.17
 
@@ -30,8 +30,6 @@ SET time_zone = "+00:00";
 CREATE TABLE `reservation_list` (
   `reservation_id` int(11) NOT NULL,
   `customer_id` varchar(200) NOT NULL,
-  `contact` text NOT NULL,
-  `email` text NOT NULL,
   `table_id` int(11) NOT NULL,
   `datetime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `status` int(11) NOT NULL DEFAULT 0,
@@ -42,12 +40,13 @@ CREATE TABLE `reservation_list` (
 -- Đang đổ dữ liệu cho bảng `reservation_list`
 --
 
-INSERT INTO `reservation_list` (`reservation_id`, `customer_id`, `contact`, `email`, `table_id`, `datetime`, `status`, `date_created`) VALUES
-(11, '1', '123', 'npt5767@gmail.com', 1, '2023-12-13 22:00:00', 0, '2023-12-13 20:14:23'),
-(12, '1', '123', 'npt5767@gmail.com', 2, '2023-12-13 22:00:00', 0, '2023-12-13 20:15:27'),
-(13, '1', '123', 'npt5767@gmail.com', 3, '2023-12-13 22:00:00', 0, '2023-12-13 20:15:30'),
-(14, '1', '123', 'npt5767@gmail.com', 4, '2023-12-13 22:00:00', 0, '2023-12-13 20:15:31'),
-(15, '1', '123', 'npt5767@gmail.com', 5, '2023-12-13 22:00:00', 0, '2023-12-13 20:15:32');
+INSERT INTO `reservation_list` (`reservation_id`, `customer_id`, `table_id`, `datetime`, `status`, `date_created`) VALUES
+(11, '1', 1, '2023-12-13 22:00:00', 0, '2023-12-13 20:14:23'),
+(12, '1', 2, '2023-12-13 22:00:00', 0, '2023-12-13 20:15:27'),
+(13, '1', 3, '2023-12-13 22:00:00', 0, '2023-12-13 20:15:30'),
+(14, '1', 4, '2023-12-13 22:00:00', 0, '2023-12-13 20:15:31'),
+(25, '1', 6, '0000-00-00 00:00:00', 0, '2023-12-22 12:56:46'),
+(26, '17', 6, '2023-11-28 12:56:00', 0, '2023-12-22 13:07:38');
 
 -- --------------------------------------------------------
 
@@ -61,19 +60,72 @@ CREATE TABLE `table_list` (
   `table_name` text DEFAULT NULL,
   `description` text DEFAULT NULL,
   `status` int(11) NOT NULL DEFAULT 1,
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `size` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `table_list`
 --
 
-INSERT INTO `table_list` (`table_id`, `tbl_no`, `table_name`, `description`, `status`, `date_created`) VALUES
-(1, 1, 'Table 1', 'Description for Table 1', 1, '2023-12-12 15:32:25'),
-(2, 2, 'Table 2', 'Description for Table 2', 1, '2023-12-12 15:34:57'),
-(3, 3, 'Table 3', 'Description for Table 3', 1, '2023-12-12 15:34:57'),
-(4, 4, 'Table 4', 'Description for Table 4', 1, '2023-12-12 15:34:57'),
-(5, 5, 'Table 5', 'Description for Table 5', 1, '2023-12-12 15:34:57');
+INSERT INTO `table_list` (`table_id`, `tbl_no`, `table_name`, `description`, `status`, `date_created`, `size`) VALUES
+(6, 1, 'T101', NULL, 1, '2023-12-21 14:40:05', 8),
+(7, 2, 'T102', NULL, 1, '2023-12-21 14:40:05', 8),
+(8, 3, 'T103', NULL, 1, '2023-12-21 14:40:05', 8),
+(9, 4, 'T104', NULL, 1, '2023-12-21 14:40:05', 2),
+(10, 5, 'T105', NULL, 1, '2023-12-21 14:40:05', 2),
+(11, 6, 'T106', NULL, 1, '2023-12-21 14:40:05', 2),
+(12, 7, 'T107', NULL, 1, '2023-12-21 14:40:05', 4),
+(13, 8, 'T108', NULL, 1, '2023-12-21 14:40:05', 4),
+(14, 9, 'T109', NULL, 1, '2023-12-21 14:40:05', 4),
+(15, 10, 'T1010', NULL, 1, '2023-12-21 14:40:05', 4),
+(16, 11, 'T1011', NULL, 1, '2023-12-21 14:40:05', 2),
+(17, 12, 'T1012', NULL, 1, '2023-12-21 14:40:05', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `verify_token` varchar(255) NOT NULL,
+  `verify_status` tinyint(2) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id`, `fullname`, `email`, `password`, `phone`, `role`, `verify_token`, `verify_status`) VALUES
+(1, 'Song Cat', 'admin123@gmail.com', '$2y$10$E6PpQm2UUUNB6M5XZ7vhzOxuLmdsPiGIZz58Zyq95x3lsidBSq8im', '0901281211', 'Admin', '', 1),
+(7, 'qeqweqe', 'admin@mail.eqewqe', '123', '0376966096', 'Customer', 'a5c8076885b40f5221cf45507178a05d', 0),
+(9, 'HIIIIII', 'admin@mail', '123', '0192121311', 'Customer', 'c3324ec68667865d3ee68390e9fa76f1', 0),
+(10, 'Anh Chi', 'nlachi21@clc.fitus.edu.vn', '123456', '0376966096', 'Customer', '5479e3b77042d6eb05b92e532a95c8cb', 0),
+(11, 'Song Cat', 'lnscat21@clc.fitus.edu.vn', '123', '11212121', 'Customer', '014d5fce729ecbb08496f0d6d60c9d1c', 0),
+(14, 'Song Cat', 'songcatlengo.learning@gmail.com', '$2y$10$E6PpQm2UUUNB6M5XZ7vhzOxuLmdsPiGIZz58Zyq95x3lsidBSq8im', '0123456789', 'Customer', 'e95bbe0c2844af3f1cf08f3209c0a3f9', 1),
+(17, 'Staff21', 'staff12345@gmail.com', '$2y$10$FUML9jrQPf2a0j4/90IY4uMrKMi9QWATBzRBxtRM0zNSRkXw/9SRi', '0921821274', 'Staff', '', 1),
+(18, 'Staff SC', 'staffSC@gmail.com', '$2y$10$1iypvtIR786lQ404yZtRq.8Yv8zEFLSToPv1aWlMetKAh5sODu6v6', '0912123761', 'Staff', '', 1),
+(19, 'Staff Nhi', 'staffNhi@gmail.com', '$2y$10$JwdAlaEeq3Ei94GW8O1W4eEKYEMIG44DfGu5cfgIbhXyHVIFsEz9.', '0903228329', 'Staff', '', 1),
+(20, 'Staff15', 'staff15@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(21, 'Staff16', 'staff16@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(22, 'Staff17', 'staff17@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(23, 'Staff18', 'staff18@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(24, 'Staff19', 'staff19@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(25, 'Staff20', 'staff20@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(26, 'Staff21', 'staff21@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(27, 'Staff22', 'staff22@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(28, 'Staff23', 'staff23@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(29, 'Staff24', 'staff24@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(30, 'Staff25', 'staff25@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(31, 'Staff26', 'staff26@gmail.com', '123', '0903333333', 'Staff', '', 1),
+(32, 'Tai Nguyen', 'npt5767@gmail.com', '$2y$10$2gx95HW57EjfqbxtQbqylO5JLsNmxSCyINJiGK6mm5d/hq1LfSrzC', '+84377457651', 'Customer', '9c7e1dac7e4fad7860f078cf5a4ca803', 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -92,6 +144,12 @@ ALTER TABLE `table_list`
   ADD PRIMARY KEY (`table_id`);
 
 --
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
 
@@ -99,13 +157,19 @@ ALTER TABLE `table_list`
 -- AUTO_INCREMENT cho bảng `reservation_list`
 --
 ALTER TABLE `reservation_list`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT cho bảng `table_list`
 --
 ALTER TABLE `table_list`
-  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
