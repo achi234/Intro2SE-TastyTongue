4cebb2290772995b0c90fa9d665ad063cc84a2c6
anhchi7103 <nlachi21@clc.fitus.edu.vn>
2023-12-24 19:00:42 +0700

    update partials for StaffUI

---
+++ src/Staff UI/partials/_sidebar.php
@@ -0,0 +1,89 @@
+<var class="navbar navbar-vertical navbar-expand-md">
+    <div class="container">
+        <!-- Brand -->
+        <a href="dashboard.php" class="navbar__header">
+            <img src="../assets/image/logo_removebg-2.png" alt="TastyTongue.png" class="navbar-header__logo">
+            <!-- <div class="navbar-header__logo" style="background-image: image(../assets/image/logo_removebg-2.png)"></div> -->
+            <h1 class="navbar-header__name">Tasty Tongue</h1>
+        </a>
+        <!-- Divider -->
+        <hr class="navbar__divider">
+        <div class="navbar__collapse">
+            <!-- Navigation -->
+            <ul class="navbar-nav">
+                <li class="navbar-nav__item">
+                    <a href="dashboard.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-tv nav-item__icon"></i>
+                        <p class="nav-item__text">Dashboard</p>
+                    </a>
+                </li>
+                <li class="navbar-nav__item">
+                    <a href="customers.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-users nav-item__icon"></i>
+                        <p class="nav-item__text">Customers</p>
+                    </a>
+                </li>
+                <li class="navbar-nav__item">
+                    <a href="products.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-utensils nav-item__icon"></i>
+                        <p class="nav-item__text">Products</p>
+                    </a>
+                </li>
+                <li class="navbar-nav__item">
+                    <a href="orders.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-cart-shopping nav-item__icon"></i>
+                        <p class="nav-item__text">Orders</p>
+                    </a>
+                </li>
+                <li class="navbar-nav__item">
+                    <a href="payments.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-credit-card nav-item__icon"></i>
+                        <p class="nav-item__text">Payments</p>
+                    </a>
+                </li>
+                <li class="navbar-nav__item">
+                    <a href="receipts.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-file-invoice nav-item__icon"></i>
+                        <p class="nav-item__text">Receipts</p>
+                    </a>
+                </li>
+                <li class="navbar-nav__item">
+                    <a href="reservationManage.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa fa-calendar nav-item__icon"></i>
+                        <p class="nav-item__text">Reservation</p>
+                    </a>
+                </li>
+            </ul>
+            <!-- Divider -->
+            <hr class="navbar__divider">
+            <!-- Tittle -->
+            <h4 class="navbar__tittle">REPORTING</h4>
+            <!-- Navigation -->
+            <ul class="navbar-nav mb-md-3">
+                <li class="navbar-nav__item">
+                    <a href="order_records.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-basket-shopping nav-item__icon"></i>
+                        <p class="nav-item__text">Orders</p>
+                    </a>
+                </li>
+                <li class="navbar-nav__item">
+                    <a href="payment_records.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-filter-circle-dollar nav-item__icon"></i>
+                        <p class="nav-item__text">Payments</p>
+                    </a>
+                </li>
+            </ul>
+            <!-- Divider -->
+            <hr class="navbar__divider">
+            <!-- Logout -->
+            <ul class="navbar-nav mb-md-3">
+                <li class="navbar-nav__item">
+                    <a href="login.php" class="nav-item__link text-danger">
+                        <i class="fa-solid fa-right-from-bracket nav-item__icon"></i>
+                        <p class="nav-item__text">Log Out</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</var>
---
+++ src/Staff UI/partials/_topnav.php
@@ -0,0 +1,32 @@
+<div class="navbar navbar-top navbar-expand-md">
+    <div class="container container-header">
+        <a href="dashboard.php" class="h4 container__dashboard-name">STAFF DASHBOARD</a>
+
+        <li class="navbar-user">
+            <img alt="Image placeholder" src="../assets/image/profileImage.png" alt="" class="navbar-user-img">
+            <span class="navbar-user-name">
+                <?php
+                    echo $_SESSION['auth_user']['fullname'];
+                ?>
+            </span>
+
+            <ul class="navbar-user-menu">
+                <li class="navbar-nav__item">
+                    <a href="change_profile.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-user nav-item__icon"></i>
+                        <p class="nav-item__text">My profile</p>
+                    </a>
+                </li>  
+                
+                <hr class="">
+                
+                <li class="navbar-nav__item">
+                    <a href="login.php" class="nav-item__link text-primary">
+                        <i class="fa-solid fa-person-running nav-item__icon"></i>
+                        <p class="nav-item__text">Logout</p>
+                    </a>
+                </li>                       
+            </ul>
+        </li>
+    </div>
+</div>
