7424ca9d52dadae999831cbc6dee3ef2252a2f9c
BNKieuNhi <93073321+BNKieuNhi@users.noreply.github.com>
2023-12-31 14:54:48 +0700

    Update order_records.php

--- src/Admin UI/order_records.php
+++ src/Admin UI/order_records.php
@@ -2,6 +2,7 @@
     $page_title = "Tasty Tongue - Order List";
     require_once('partials/_head.php');
     //require_once('partials/_analytics.php');
+    // $reservation_id = $_GET['reservation_id'];
 ?>
 <body>
     <!-- Sidebar -->
@@ -16,6 +17,7 @@
             require_once('partials/_topnav.php');
             ?>
             <!-- Page content -->
+            <!-- Order Records -->
             <div class="container">
                 <div class="container-recent">
                     <div class="container-recent-inner">
@@ -27,42 +29,35 @@
                             <table class="table">
                                 <thead class="thead-light">
                                     <tr>
-                                        <th class="text-column-emphasis" scope="col">CODE</th> 
-                                        <th class="text-column" scope="col">CUSTOMER</th> 
-                                        <th class="text-column-emphasis" scope="col">PRODUCT</th> 
+                                        <th class="text-column-emphasis" scope="col">CUSTOMER</th> 
+                                        <th class="text-column" scope="col">PRODUCT</th> 
                                         <th class="text-column" scope="col">UNIT PRICE</th> 
-                                        <th class="text-column-emphasis" scope="col">QTY</th> 
+                                        <th class="text-column" scope="col">QTY</th> 
                                         <th class="text-column" scope="col">TOTAL</th> 
-                                        <th class="text-column" scope="col">STATUS</th> 
-                                        <th class="text-column-emphasis" scope="col">DATE</th> 
+                                        <th class="text-column" scope="col">ACTION</th> 
                                     </tr>
                                 </thead>
                                 <tbody class="table-body">
                                     <tr>
-                                        <th class="text-column-emphasis" scope="row">JFMB-0731</th> 
-                                        <th class="text-column" scope="row">Christine Moore</th> 
-                                        <th class="text-column-emphasis" scope="row">Irish Coffee</th> 
+                                        <th class="text-column-emphasis" scope="row">Christine Moore</th> 
+                                        <th class="text-column" scope="row">Irish Coffee</th> 
                                         <th class="text-column" scope="row">$11</th> 
-                                        <th class="text-column-emphasis" scope="row">1</th> 
+                                        <th class="text-column" scope="row">1</th> 
                                         <th class="text-column" scope="row">$11</th> 
-                                        <th class="text-column-emphasis" scope="row">
-                                            <span class="badge badge-success">Paid</span>
-                                        </th> 
-                                        <th class="text-column-emphasis" scope="row">04/Sep/2022 11:37</th> 
+                                        <th class="text-column" scope="row">
+                                            <div class="text-column__action">
+                                                <a href="" class="btn-control btn-control-delete">
+                                                    <i class="fa-solid fa-trash-can btn-control-icon"></i>
+                                                    Delete
+                                                </a>
+                                                <a href="update_order_records.php?id=<?php  //echo $reservation['reservation_id']?>" class="btn-control btn-control-edit">
+                                                    <i class="fa-solid fa-pen-to-square btn-control-icon"></i>
+                                                    Update
+                                                </a>
+                                            </div>
+                                        </th>
                                     </tr>
 
-                                    <tr>
-                                        <th class="text-column-emphasis" scope="row">JFMB-0731</th> 
-                                        <th class="text-column" scope="row">Christine Moore</th> 
-                                        <th class="text-column-emphasis" scope="row">Irish Coffee</th> 
-                                        <th class="text-column" scope="row">$11</th> 
-                                        <th class="text-column-emphasis" scope="row">1</th> 
-                                        <th class="text-column" scope="row">$11</th> 
-                                        <th class="text-column-emphasis" scope="row">
-                                            <span class="badge badge-success">Paid</span>
-                                        </th> 
-                                        <th class="text-column-emphasis" scope="row">04/Sep/2022 11:37</th> 
-                                    </tr>
                                 </tbody>
                             </table>
 
